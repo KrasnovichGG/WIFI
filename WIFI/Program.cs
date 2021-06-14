2a08e794e7a717abf0d4048789b0244ab330e86b
Александр Краснов <krsasha200393@gmail.com>
2021-06-14 13:12:52 +0300

    Добавьте файлы проекта.

---
+++ WIFI/Program.cs
@@ -0,0 +1,44 @@
+﻿// OpenWeatherMap REST API example
+// DKY 2021
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WIFI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            var ApiKey = "d3f63b4b55db0b86ef4eba7539e07204";
+            //var City = "Kazan";
+            var url = $"https://apidata.mos.ru/v1/datasets/2756/rows?api_key={ApiKey}";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                var WIFIForecast = JsonConvert.DeserializeObject<List<Root>>(result);
+
+                
+            }
+
+        }
+    }
+}
---
+++ WIFI/Root.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIFI
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class GeoData
+    {
+        public List<double> coordinates { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Cells
+    {
+        public string Name { get; set; }
+        public string AdmArea { get; set; }
+        public string District { get; set; }
+        public string Location { get; set; }
+        public int NumberOfAccessPoints { get; set; }
+        public string WiFiName { get; set; }
+        public int CoverageArea { get; set; }
+        public string FunctionFlag { get; set; }
+        public string AccessFlag { get; set; }
+        public string Password { get; set; }
+        public int global_id { get; set; }
+        public GeoData geoData { get; set; }
+    }
+
+    public class Root
+    {
+        public int global_id { get; set; }
+        public int Number { get; set; }
+        public Cells Cells { get; set; }
+    }
+
+
+
+}
