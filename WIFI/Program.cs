7709903eeb79ae1c2f1b2559d322c8383fd7d475
Александр Краснов <krsasha200393@gmail.com>
2021-06-21 11:03:56 +0300

    Улучшил телеграм бота

--- WIFI/Program.cs
+++ WIFI/Program.cs
@@ -1,18 +1,19 @@
-﻿
-
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text;
-using Newtonsoft.Json;
+using Telegram.Bot;
 
-namespace WIFI
+namespace TelegramBot
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Root> WifiList;
             Console.OutputEncoding = Encoding.Unicode;
             var ApiKey = "d3f63b4b55db0b86ef4eba7539e07204";
             //var City = "Kazan";
@@ -32,12 +33,27 @@ namespace WIFI
             using (var streamReader = new StreamReader(response.GetResponseStream()))
             {
                 string result = streamReader.ReadToEnd();
-                Console.WriteLine(result);
-                var WIFIForecast = JsonConvert.DeserializeObject<List<Root>>(result);
-
-                
+                WifiList = JsonConvert.DeserializeObject<List<Root>>(result);
             }
 
+
+            TelegramBotClient bot = new TelegramBotClient("1769451705:AAFDJB7I0fUwjzdLxtFXJqss_dl9CJI9rd4");
+            bot.OnMessage += (s, arg) =>
+            {
+                string msg;
+                var wifi = WifiList.Where(it => it.Cells.Location.Contains(arg.Message.Text)).Select(item => $"Название = {item.Cells.WiFiName}\nУлица = {item.Cells.Location}").ToArray();
+                if (arg.Message.Text == "/start")
+                    msg = "Введите улицу Москвы! \n Например : Басманная улица, Кутузовский проспект, Киевская улица, Ленинградский проспект , Пятницкая улица, Трёхпрудный переулок";
+                else if (wifi.Length > 0)
+                    msg = string.Join("\n", wifi);
+                else
+                    msg = "Напишите в чат команду /start";
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"Бот говорит:\n { msg }");
+            };
+
+            bot.StartReceiving();
+            Console.ReadKey();
         }
     }
 }
